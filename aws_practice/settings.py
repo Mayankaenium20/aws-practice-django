70adf1ea3184e2edf75bfb0e66a7230c5649383a
MayankbECI <mayank.b@ecinfosolutions.com>
2026-01-04 02:10:33 +0530

    removed the unwanted session authentication.

--- aws_practice/settings.py
+++ aws_practice/settings.py
@@ -132,8 +132,7 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
